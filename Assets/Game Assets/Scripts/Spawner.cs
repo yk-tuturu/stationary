6433a6dbe06e74503b6960792b74cd93170d528f
NownuJ <junwon2178@gmail.com>
2024-09-14 16:18:25 +0800

    basic structure
    item coming down, health decrease, gamer over screen

--- Assets/Game Assets/Scripts/Spawner.cs
+++ Assets/Game Assets/Scripts/Spawner.cs
@@ -1,61 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.EventSystems;
 
-public class EnemySpawner : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private GameObject[] enemyprefabs;
 
     [Header("Attributes")]
-    [SerializeField] private int baseEnemies = 8;
-    [SerializeField] private float enemiesPerSecond = 0.5f;
-    [SerializeField] private float timeBetweenWaves = 5f;
-    [SerializeField] private float ScalingFactor = 0.75f;
-    [SerializeField] private float enemiesPerSecondCap = 10f;
-
-    [Header("Events")]
-    public static UnityEvent onEnemyDestory = new UnityEvent();
+    [SerializeField] private int baseEnemies = 20;
+    [SerializeField] private float enemiesPerSecond = 0.3f;
 
 
-    private int currWave = 1;
     private float timeSinceLastSpawn;
-    private int enemiesAlive;
     private int enemiesLeftToSpawn;
-    private float eps; //enemies per second
     private bool isSpawning = false;
-
-
-    private void Awake()
-    {
-        onEnemyDestory.AddListener(EnemyDestroyed);
-    }
 
     private void Start()
     {
-        StartCoroutine(StartWave());
+        isSpawning = true;
+        enemiesLeftToSpawn = baseEnemies;
     }
 
-    private void Update()
+    void Update()
     {
+        if (GameManager.instance.gamePause) return;
         if (!isSpawning) return;
 
         timeSinceLastSpawn += Time.deltaTime;
 
-        if (timeSinceLastSpawn >= (1f / eps) && enemiesLeftToSpawn > 0)
+        if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeftToSpawn > 0)
         {
             SpawnEnemy();
             enemiesLeftToSpawn--;
-            enemiesAlive++;
-            timeSinceLastSpawn = 0f;
-        }
 
-        if (enemiesAlive == 0 && enemiesLeftToSpawn == 0)
-        {
-            EndWave();
+            timeSinceLastSpawn = 0f;
         }
     }
 
@@ -63,40 +42,6 @@ public class EnemySpawner : MonoBehaviour
     {
         int index = Random.Range(0, enemyprefabs.Length);
         GameObject prefabToSpawn = enemyprefabs[index];
-        Instantiate(prefabToSpawn, LevelManager.instance.startPoint.position, Quaternion.identity);
+        Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
     }
-
-
-    private IEnumerator StartWave()
-    {
-        yield return new WaitForSeconds(timeBetweenWaves);
-        isSpawning = true;
-        enemiesLeftToSpawn = EnemiesPerWave();
-        eps = EnemiesPerSecond();
-    }
-
-    private void EnemyDestroyed()
-    {
-        enemiesAlive--;
-    }
-
-    private int EnemiesPerWave()
-    {
-        return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currWave, ScalingFactor));
-    }
-
-    private float EnemiesPerSecond()
-    {
-        return Mathf.Clamp(enemiesPerSecond * Mathf.Pow(currWave, ScalingFactor), 0f, enemiesPerSecondCap);
-    }
-
-    private void EndWave()
-    {
-        isSpawning = false;
-        timeSinceLastSpawn = 0f;
-        currWave++;
-        StartCoroutine(StartWave());
-    }
-
-
 }
